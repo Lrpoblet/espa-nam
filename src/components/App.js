8c25d820b9c7bb07dd7f15c4c332d9b3dd1b29b2
Lara <lrpoblet@gmail.com>
2022-12-15 20:47:23 +0100

    add project structure and background image and logo

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+// import {useEffect, useState} from 'react';
+// import {Route} from 'react-router-dom';
+// import callToApi from '../services/api';
+// import ls from '../services/localStorage';
+
+import '../styles/App.scss';
+import logo from '../images/Logo.png';
+
+function App() {
+  // VARIABLES ESTADO
+
+  // USEEFFECT
+
+  // FUNCIONES HANDLER
+
+  // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR HTML
+
+  // HTML EN EL RETURN
+
+  return (
+    <div className="container">
+      <header>
+        <img className="logo" src={logo} alt="Espa-¡ñam!" title="Espa-¡ñam!" />
+      </header>
+    </div>
+  );
+}
+
+export default App;

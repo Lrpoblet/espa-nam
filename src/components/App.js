175c94ed8cc3a87cf971b36c99c7cc8fb0ba0568
Lara <lrpoblet@gmail.com>
2022-12-19 16:36:08 +0100

    layout structure and add some maps

--- src/components/App.js
+++ src/components/App.js
@@ -19,8 +19,33 @@ function App() {
 
   return (
     <div className="container">
-      <header>
+      <header className="header">
         <img className="logo" src={logo} alt="Espa-¡ñam!" title="Espa-¡ñam!" />
+        <form action="" className="search">
+          <select name="" id="select">
+            <option selected="true" disabled="disabled" value="">
+              Selecciona Comunidad Autónoma
+            </option>
+            <option value="Andalucía">Andalucía</option>
+            <option value="Aragón">Aragón</option>
+            <option value="Asturias">Asturias</option>
+            <option value="Baleares">Islas Baleares</option>
+            <option value="Canarias">Islas Canarias</option>
+            <option value="Cantabria">Cantabria</option>
+            <option value="Castilla La-Mancha">Castilla La-Mancha</option>
+            <option value="Castilla y León">Castilla y León</option>
+            <option value="Cataluña">Cataluña</option>
+            <option value="Comunidad Valenciana">Comunidad Valenciana</option>
+            <option value="Extremadura">Extremadura</option>
+            <option value="Galicia">Galicia</option>
+            <option value="Comunidad de Madrid">Comunidad de Madrid</option>
+            <option value="Murcia">Murcia</option>
+            <option value="Navarra">Navarra</option>
+            <option value="País Vasco">País Vasco</option>
+            <option value="La Rioja">La Rioja</option>
+          </select>
+          {/* <input type="button" value="Search" className="search__button" /> */}
+        </form>
       </header>
     </div>
   );
